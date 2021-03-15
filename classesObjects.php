6d38d8e7a2a28741ad341d265641be77ab596b95
Tacey127 <jtacey@hotmail.co.uk>
2021-03-15 17:27:10 +0000

    33/33

--- classesObjects.php
+++ classesObjects.php
@@ -10,14 +10,48 @@
     <?php
     
     class Book {
-        var $title;
+        public $title;
         var $author;
-        var $pages;
+        private $pages;
+
+        function __construct($_title)
+        {
+          echo "ctor";
+          $this->title = $_title;
+        }
+
+        function speak()
+        {
+          echo $this->title;
+        }
+
+        function setPages($num)
+        {
+          $this->pages = $num;
+        }
+
+        function getPages()
+        {
+          return $this->pages;
+        }
+
     }
 
 
-    $book1 = new Book;
+    $book1 = new Book("au");
     $book1->title = "auth";
+
+
+    //inheritance
+    class WierdBook extends Book
+    {
+        function speak()
+        {
+          echo "auaudf";
+        }
+    }
+
+
 
     ?>
 
